b7eaa510fa17cf7500dabbca4ed548bada527086
adhyan <adhyanmahajan1999@gmail.com>
2026-05-26 20:00:10 +0530

    solved 121. Best Time to Buy and Sell Stock -arrays -easy

---
+++ Arrays/121. Best Time to Buy and Sell Stock.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int left=0;
+        int right=1;
+        int profit=0;
+        int max=0;
+
+        while(right<prices.length)
+        {
+
+            if(prices[left]<prices[right])
+            {
+                profit=prices[right]-prices[left];
+                if(max<profit)
+                {
+                    max=profit;
+                }
+            }
+            else
+            {
+                left=right;
+            }
+            right++;
+            
+        }
+        return max;
+    }
+}
